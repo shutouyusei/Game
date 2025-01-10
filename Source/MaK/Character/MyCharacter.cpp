0ccdacaf65c55f67819597b79205137b08a7e586
首藤優誠 <111882261+shutouyusei@users.noreply.github.com>
2025-01-10 13:55:46 +0900

    [add]MyCharacter

---
+++ Source/MaK/Character/MyCharacter.cpp
@@ -0,0 +1,1 @@
+#include "MyCharacter.h"
---
+++ Source/MaK/Character/MyCharacter.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "MaKCharacter.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class AMyCharacter : public AMaKCharacter {
+  GENERATED_BODY()
+};
